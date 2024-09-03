bf7b4b0520a952b545dce57b1c1da49c79e7de35
legacychimera247 <mad_fer_it@vivaldi.net>
2024-09-03 13:33:06 +0200

    Add files via upload

---
+++ translations/exawake-it.ts
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <source>Made by Michał Szczepaniak.</source>
+        <translation>Sviluppato da Michał Szczepaniak.</translation>
+    </message>
+    <message>
+        <source>Licensed under GPLv3</source>
+        <translation>Sotto licenza GPLv3</translation>
+    </message>
+    <message>
+        <source>Links</source>
+        <translation>Collegamenti</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>Dona</translation>
+    </message>
+    <message>
+        <source>Greg Goncharov (@gregguh at telegram) for icons.</source>
+        <translation>Greg Goncharov (@gregguh su telegram) per le icone.</translation>
+    </message>
+    <message>
+        <source>Thanks to</source>
+        <translation>Grazie a</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Exawake</source>
+        <translation>Exawake</translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <source>Add server</source>
+        <translation>Aggiungi server</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Modifica</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Elimina</translation>
+    </message>
+    <message>
+        <source>Servers</source>
+        <translation>Server</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Info</translation>
+    </message>
+</context>
+</TS>
